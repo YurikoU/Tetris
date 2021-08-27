394ba8308480dd2590b54259f1099b416da18a5a
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-08-27 16:38:11 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,29 +2,34 @@
 
 
 
+//Block size (px)
+const BLOCK_SIZE = 30;
+
+//Tetromino size (px)
+const TETROMINO_SIZE = 4;
+
 let $canvas = document.getElementById("can");
 let context = $canvas.getContext("2d");
-const BLOCK_SIZE     = 30;
-const TETROMINO_SIZE = 4;
+
+//Array to express four blocks * four blocks 
+//0 is empty space, 1 has a block.
 let tetromino = [
-    [ 0, 0, 0, 0 ],//0 means NO block, 1 means a block.                  
+    [ 0, 0, 0, 0 ],
     [ 1, 1, 0, 0 ],                  
     [ 0, 1, 1, 0 ],                  
     [ 0, 0, 0, 0 ],                  
 ];
 
-// context.fillStyle = "red";
-// context.fillRect( 0, 0, BLOCK_SIZE, BLOCK_SIZE );//Draw a square with BLOCK_SIZE at (0,0) coordinate
 
 for ( let y = 0; y < TETROMINO_SIZE; y++ ) {
     for ( let x = 0; x < TETROMINO_SIZE; x++ ) {
-        if ( tetromino[y][x] ) {
-            let px = x * BLOCK_SIZE;
-            let py = y * BLOCK_SIZE;
+        if ( tetromino[y][x] != 0 ) {
+            let printX = x * BLOCK_SIZE;
+            let printY = y * BLOCK_SIZE;
             context.fillStyle = "red";
-            context.fillRect( px, py, BLOCK_SIZE, BLOCK_SIZE );//Draw blocks in "Z" with BLOCK_SIZE at (px,py) coordinate
-            context.strokeStyle = "black";
-            context.strokeRect( px, py, BLOCK_SIZE, BLOCK_SIZE );
+            context.fillRect( printX, printY, BLOCK_SIZE, BLOCK_SIZE );//Draw blocks in "Z" with BLOCK_SIZE at (printX,printY) coordinate
+            context.strokeStyle = "black";//Draw a frame
+            context.strokeRect( printX, printY, BLOCK_SIZE, BLOCK_SIZE );
         }
     }
 } 
